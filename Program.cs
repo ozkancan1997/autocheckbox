b3dc825fd87ac7b0bc482fc79f04c63e21ee3b2e
voyager <ozkan.can@aol.com>
2023-03-04 23:40:39 +0300

    Options class added. Necessary methods added and partially tested. Unit test required.

--- Program.cs
+++ Program.cs
@@ -2,23 +2,50 @@
 
 Ruleset rs = new Ruleset();
 
-// rs.AddDep('A', 'B');
-// rs.AddDep('C', 'A');
 
-rs.AddDep('A', 'B');
-rs.AddDep('B', 'C');
-rs.AddDep('C', 'D');
-rs.AddDep('D','B');
-rs.AddConflict('A','C');
+// rs.AddDep('a', 'b');
+// rs.AddDep('b', 'c');
+// rs.AddDep('c', 'a');
+// rs.AddDep('d', 'e');
+// rs.AddConflict('c', 'e');
 
-Console.WriteLine(rs);
+// Options ops = new Options(rs);
+
+// ops.Toggle('a');
+// ops.StringSlice();
+
+// rs.AddDep('f', 'f');
+// ops.Toggle('f');
+// ops.StringSlice();
+
+// ops.Toggle('e');
+// ops.StringSlice();
+
+// ops.Toggle('b');
+// ops.StringSlice();
+
+// rs.AddDep('b','g');
+// ops.Toggle('g');
+// ops.Toggle('b');
+// ops.StringSlice();
+
+rs.AddDep('a', 'b');
+rs.AddDep('a', 'c');
+
+rs.AddConflict('b', 'd');
+rs.AddConflict('b', 'e');
 
 Console.WriteLine(rs.isCoherent());
 
+Options ops = new Options(rs);
+
+ops.Toggle('d'); ops.Toggle('e'); ops.Toggle('a');
+ops.StringSlice();
 
 public class Ruleset{
-    Dictionary<char, string> dependencies;
-    List<string> conflicts;
+
+    public Dictionary<char, string> dependencies{get;}
+    public List<string> conflicts{get;}
     string a="";
 
     public Ruleset(){
@@ -30,13 +57,19 @@ public class Ruleset{
         if(!dependencies.Keys.Contains(baseOpt)){
             dependencies.Add(baseOpt,"");
         }
-        dependencies[baseOpt]+=targetOpt;
+        if(!dependencies[baseOpt].Contains(targetOpt)){
+            dependencies[baseOpt]+=targetOpt;
+        }
         if(dependencies.Keys.Contains(targetOpt)){
-            dependencies[baseOpt]+=dependencies[targetOpt];
+            foreach(char c in dependencies[targetOpt]){
+                if(!dependencies[baseOpt].Contains(c)){
+                    dependencies[baseOpt]+=c;
+                }
+            }
         }
         foreach(var dep in dependencies){
             if(dep.Value.Contains(baseOpt)){
-                dependencies[dep.Key]+=dependencies[baseOpt];
+                dependencies[dep.Key]+=targetOpt;
             }
         }
 
@@ -64,10 +97,79 @@ public class Ruleset{
 
     public bool isCoherent(){
         foreach(string c in conflicts){
-            if(dependencies[c[0]].Contains(c[1]) || dependencies[c[1]].Contains(c[0])){
+            if(dependencies.Keys.Contains(c[0]) && dependencies[c[0]].Contains(c[1])){
+                return false;
+            }else if(dependencies.Keys.Contains(c[1]) && dependencies[c[1]].Contains(c[0])){
                 return false;
             }
+            foreach(string v in dependencies.Values){
+                if(v.Contains(c[0]) && v.Contains(c[1])){
+                    return false;
+                }
+            }
         }
+        
         return true;
     }
+
+}
+
+public class Options{
+    Ruleset ruleset;
+    List<char> optionList;
+
+    char prev;
+
+    public Options(){
+        this.ruleset = new Ruleset();
+        this.optionList = new List<char>();
+    }
+    public Options(Ruleset ruleset){
+        this.ruleset = ruleset;
+        this.optionList = new List<char>();
+    }
+
+    public void Toggle(char opt){
+        if(!optionList.Contains(opt)){
+            optionList.Add(opt);
+            if(ruleset.dependencies.Keys.Contains(opt)){
+                foreach(char d in ruleset.dependencies[opt]){
+                    if(!optionList.Contains(d)){
+                        Toggle(d);
+                    }
+                    
+                }
+            }
+
+        foreach(string c in ruleset.conflicts){
+            if(c[0]==opt && optionList.Contains(c[1])){
+                Toggle(c[1]);
+            }else if(c[1]==opt && optionList.Contains(c[0])){
+                Toggle(c[0]);
+            }
+        }
+        return;
+        }
+
+        if(optionList.Contains(opt)){
+            optionList.Remove(opt);
+            foreach(var dep in ruleset.dependencies){
+                if(dep.Value.Contains(opt)){
+                    optionList.Remove(dep.Key);
+                }
+            }
+        }
+
+    }
+
+    public string StringSlice(){
+        string temp="";
+        foreach(char o in optionList){
+            temp+=o+" ";
+        }
+        Console.WriteLine(temp+"\n");
+        return temp;
+    }
+
+
 }
